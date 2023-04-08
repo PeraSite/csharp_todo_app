4b9870a2ac53b2f758d84a7f20a66da2ee1dcf77
PeraSite <pretocki3@gmail.com>
2023-04-09 03:05:19 +0900

    feat: 가장 표면의 로직을 수행하는 TodoRepository 추가

---
+++ todo_app/repository/TodoRepository.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.MySqlClient;
+using todo_app.database;
+using todo_app.entity;
+
+namespace todo_app.repository;
+
+public class TodoRepository {
+	private readonly MySqlDatabase _database;
+
+	public TodoRepository(MySqlDatabase database) {
+		_database = database;
+	}
+
+	public List<User> GetAllUsers() {
+		using MySqlDataReader reader = _database.Execute("SELECT * FROM todo");
+		var users = new List<User>();
+		while (reader.Read()) {
+			users.Add(User.FromSql(reader));
+		}
+		return users;
+	}
+}
