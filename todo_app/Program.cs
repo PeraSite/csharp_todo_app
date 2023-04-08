d1dfee937affc2b2bec9702ac684390ec35e5efc
PeraSite <pretocki3@gmail.com>
2023-04-09 02:38:56 +0900

    feat: 프로젝트 시작, SQL 테스트

---
+++ todo_app/Program.cs
@@ -0,0 +1,25 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace todo_app;
+
+internal static class Program {
+	private static void Main() {
+		MySqlConnectionStringBuilder connectionString = new MySqlConnectionStringBuilder {
+			Server = Environment.GetEnvironmentVariable("MYSQL_SERVER"),
+			Port = 8080,
+			Database = "week5",
+			UserID = "root",
+			Password = Environment.GetEnvironmentVariable("MYSQL_PASSWORD")
+		};
+
+		using MySqlConnection connection = new MySqlConnection(connectionString.ToString());
+		connection.Open();
+
+		MySqlCommand cmd = new MySqlCommand("SELECT * FROM todo", connection);
+		MySqlDataReader rdr = cmd.ExecuteReader();
+		while (rdr.Read()) {
+			Console.WriteLine(rdr["content"]);
+		}
+		rdr.Close();
+	}
+}
