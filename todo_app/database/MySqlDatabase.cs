2f7f091fd1ae1365323f88de3c4a1fd9c04e74c1
PeraSite <pretocki3@gmail.com>
2023-04-09 03:03:34 +0900

    feat: Raw SQL을 처리하는 MySqlDatabase 클래스 추가

---
+++ todo_app/database/MySqlDatabase.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace todo_app.database;
+
+public class MySqlDatabase {
+	private readonly MySqlConnection _connection;
+
+	public MySqlDatabase(
+		string server,
+		uint port,
+		string database,
+		string user,
+		string password
+	) {
+		MySqlConnectionStringBuilder connectionString = new MySqlConnectionStringBuilder {
+			Server = server,
+			Port = port,
+			Database = database,
+			UserID = user,
+			Password = password
+		};
+		_connection = new MySqlConnection(connectionString.ToString());
+		_connection.Open();
+	}
+
+	~MySqlDatabase() {
+		_connection.Close();
+	}
+
+	public MySqlDataReader Execute(string query) {
+		MySqlCommand cmd = new MySqlCommand(query, _connection);
+		MySqlDataReader rdr = cmd.ExecuteReader();
+		return rdr;
+	}
+
+	public int ExecuteNonQuery(string query) {
+		MySqlCommand cmd = new MySqlCommand(query, _connection);
+		return cmd.ExecuteNonQuery();
+	}
+}
