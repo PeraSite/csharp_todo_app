5de0ebb16554eca4f9850d92901784c54e2b44a1
PeraSite <pretocki3@gmail.com>
2023-04-09 02:59:51 +0900

    feat: SqlReader를 Entity로 변환해주는 유틸 함수 추가

--- todo_app/entity/Todo.cs
+++ todo_app/entity/Todo.cs
@@ -1,8 +1,19 @@
-﻿namespace todo_app.entity;
+﻿using MySql.Data.MySqlClient;
+
+namespace todo_app.entity;
 
 public record Todo(
 	int ID,
 	int UserID,
 	string Content,
 	bool Completed
-);
+) {
+	public static Todo FromSql(MySqlDataReader reader) {
+		return new Todo(
+			reader.GetInt32(0),
+			reader.GetInt32(1),
+			reader.GetString(2),
+			reader.GetBoolean(3)
+		);
+	}
+};
--- todo_app/entity/User.cs
+++ todo_app/entity/User.cs
@@ -1,6 +1,16 @@
-﻿namespace todo_app.entity;
+﻿using MySql.Data.MySqlClient;
+
+namespace todo_app.entity;
 
 public record User(
 	int ID,
 	string Name
-);
+) {
+
+	public static User FromSql(MySqlDataReader reader) {
+		return new User(
+			reader.GetInt32(0),
+			reader.GetString(1)
+		);
+	}
+};
