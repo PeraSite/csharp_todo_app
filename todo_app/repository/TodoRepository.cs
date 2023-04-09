7a162a769ba157f1920267d3152269a9856d9759
PeraSite <pretocki3@gmail.com>
2023-04-09 14:53:33 +0900

    feat: TodoRepository 기능 구현

--- todo_app/repository/TodoRepository.cs
+++ todo_app/repository/TodoRepository.cs
@@ -1,4 +1,5 @@
-﻿using todo_app.database;
+﻿using MySql.Data.MySqlClient;
+using todo_app.database;
 using todo_app.entity;
 
 namespace todo_app.repository;
@@ -10,16 +11,36 @@ public class TodoRepository {
 		_database = database;
 	}
 
-	public void AddTodoItem(string username, string s) {
-		throw new NotImplementedException();
+	public bool AddTodo(User user, string content) {
+		(MySqlParameter, int ID) userParameter = (new MySqlParameter("@user_id", MySqlDbType.Int32), user.ID);
+		(MySqlParameter, string content) contentParameter = (new MySqlParameter("@content", MySqlDbType.VarChar, 256), content);
+
+		var affectedRow = _database.ExecuteNonQuery("INSERT INTO todo (user_id, content, completed) VALUES (@user_id, @content, false)",
+			userParameter, contentParameter);
+
+		return affectedRow == 1;
 	}
-	public IEnumerable<Todo> GetTodoItems(string username) {
-		throw new NotImplementedException();
+
+	public List<Todo> GetAllTodo(User user) {
+		(MySqlParameter, int ID) userParameter = (new MySqlParameter("@user_id", MySqlDbType.Int32), user.ID);
+
+		using MySqlDataReader reader = _database.Execute("SELECT * FROM todo WHERE user_id = @user_id", userParameter);
+		var todos = new List<Todo>();
+		while (reader.Read()) {
+			todos.Add(Todo.FromSql(reader));
+		}
+		return todos;
 	}
-	public void SetTodoItemDone(string username, uint parse) {
-		throw new NotImplementedException();
+
+	public bool SetTodoDone(uint todoId) {
+		(MySqlParameter, uint todoId) todIdParameter = (new MySqlParameter("@todo_id", MySqlDbType.Int32), todoId);
+		var affectedRow = _database.ExecuteNonQuery("UPDATE todo SET completed = true WHERE id = @todo_id", todIdParameter);
+		return affectedRow == 1;
 	}
-	public void DeleteTodoItem(string username, uint parse) {
-		throw new NotImplementedException();
+
+	public bool DeleteTodo(uint todoId) {
+		(MySqlParameter, uint todoId) todIdParameter = (new MySqlParameter("@todo_id", MySqlDbType.Int32), todoId);
+		var affectedRow = _database.ExecuteNonQuery("DELETE FROM todo WHERE id = @todo_id", todIdParameter);
+		return affectedRow == 1;
 	}
 }
